cf50f7ff485bc4be5b65714a14e8ef9df7e62e7b
ThaisAlvarenga <69427076+ThaisAlvarenga@users.noreply.github.com>
2021-09-27 11:51:47 +0400

    Finished team section
    Finished team section and started the about section

--- script.js
+++ script.js
@@ -9,16 +9,30 @@ toggleButton.addEventListener('click', () =>{
 
 /*Script for video gallery*/
 var videoPlayer = document.getElementById("videoPlayer");
-                var showingVideo = document.getElementById("showing-video");
+var showingVideo = document.getElementById("showing-video");
 
-                function stopVideo()
-                {
-                    showingVideo.pause();
-                    videoPlayer.style.display = "none";
-                }
+function stopVideo()
+    {
+        showingVideo.pause();
+        videoPlayer.style.display = "none";
+    }
 
-                function playVideo(file)
-                {
-                    showingVideo.src = file;
-                    videoPlayer.style.display = "block";
-                }
+function playVideo(file)
+    {
+        showingVideo.src = file;
+        videoPlayer.style.display = "block";
+    }
+
+/*Script for team page*/
+
+document.addEventListener("mousemove", parallax);
+    function parallax(e){
+      document.querySelectorAll(".object").forEach(function(move){
+
+        var moving_value = move.getAttribute("data-value");
+        var x = (e.clientX * moving_value) / 250;
+        var y = (e.clientY * moving_value) / 250;
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+      });
+    }
